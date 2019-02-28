c311373190cf957a5e02dfbdcba91827502f558a
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-28 22:56:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,6 +32,8 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+
+
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
@@ -49,10 +51,6 @@ def values_for_insert
     VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
-
-  def table_name_for_insert
-    self.class.table_name
   end
 
 def self.find_by_name(name)
